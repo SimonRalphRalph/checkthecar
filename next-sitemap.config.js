02e5677461d02b04acc46e510ea8b5f7748f962d
SimonRalphRalph <93937098+SimonRalphRalph@users.noreply.github.com>
2025-08-28 00:13:01 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://checkthecar.example', // change this once you deploy
+  siteUrl: 'https://checkthecar.example',
   generateRobotsTxt: true,
-  exclude: ['/admin/*', '/api/*'],
+  exclude: ['/admin/*', '/api/*']
 }
